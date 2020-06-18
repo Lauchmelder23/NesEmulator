32a80a5dc64a906682b44c0052b350f14906c5e4
Robert <Robert-Altner@t-online.de>
2020-06-18 16:45:49 +0200

    Initial commit§

---
+++ NES Emulator/NES Emulator.cpp
@@ -0,0 +1,12 @@
+﻿// NES Emulator.cpp : Defines the entry point for the application.
+//
+
+#include "NES Emulator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
